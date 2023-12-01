98d33bbb5f02196cd1de47ab4e45953025c1ef8d
Jesse Rusak <jerusak@microsoft.com>
2023-12-01 08:53:13 -0800

    day 1 part 2

---
+++ src/bin/day1p2.rs
@@ -0,0 +1,43 @@
+use std::fs;
+
+fn value(needle: &str) -> Option<u32> {
+    if needle == "" {
+        return None;
+    }
+    if needle.chars().next().unwrap().is_ascii_digit() {
+        return Some(needle.chars().next().unwrap().to_digit(10).unwrap());
+    }
+
+    for (value, string) in [
+        (1, "one"),
+        (2, "two"),
+        (3, "three"),
+        (4, "four"),
+        (5, "five"),
+        (6, "six"),
+        (7, "seven"),
+        (8, "eight"),
+        (9, "nine"),
+    ] {
+        if needle.starts_with(string) {
+            return Some(value);
+        }
+    }
+    None
+}
+
+fn main() {
+    let mut sum = 0;
+    for line in fs::read_to_string("input/day1.txt").unwrap().lines() {
+        let substrings = line.char_indices().map(|(i, _)| &line[i..]);
+        let first_digit = substrings.clone().find_map(value).unwrap();
+        let last_digit = substrings.clone().rev().find_map(value).unwrap();
+
+        let joined = format!("{}{}", first_digit, last_digit)
+            .parse::<u32>()
+            .unwrap();
+        sum += joined;
+    }
+
+    println!("{sum}")
+}
